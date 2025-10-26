ab448cc98697e2335a84990e34ed10c8bfb85509
Avoisavo <lvera296@gmail.com>
2025-10-26 15:05:17 +0800

    edit bug

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Web3Provider } from "./providers";
+import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
